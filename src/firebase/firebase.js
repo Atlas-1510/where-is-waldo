b6aa8caa985fc19a82de84de4cedb605f981b8ef
Jason Aravanis <j.aravanis@icloud.com>
2021-07-20 19:16:49 +1000

    Add firebase config

---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+import "firebase/firestore"; // database
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBQXvgdIcNCks4yxgoIHr-ORq-8JJitenA",
+  authDomain: "my-attempt-b7181.firebaseapp.com",
+  projectId: "my-attempt-b7181",
+  storageBucket: "my-attempt-b7181.appspot.com",
+  messagingSenderId: "418299922451",
+  appId: "1:418299922451:web:4f70e172144698d5ad57e6",
+  measurementId: "G-PXQNPXD193",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const firestore = firebase.firestore();
+
+export { firestore };
