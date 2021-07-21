847e7a42973dc4711e13a71fafa23b9d35c24761
Jason Aravanis <j.aravanis@icloud.com>
2021-07-21 17:36:45 +1000

    Remove completed todos

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,52 +1,16 @@
 // Libraries
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { useLocation } from "react-router";
 
 // Components
 import GameHeader from "../components/GameHeader";
 import Image from "../components/Image";
 import VictoryModal from "../components/VictoryModal";
 
-// Assets
-// import TestImage from "../images/Zalando-Festival-Map-No-Title-Final-2.jpg"; // TODO: Remove dummy image, load from firebase instead.
-
 const Container = styled.main`
   display: flex;
   flex-direction: column;
 `;
-
-// // TODO: Remove dummy targets
-// const DummyTargets = [
-//   {
-//     emoji: "👻",
-//     title: "ghost",
-//   },
-//   {
-//     emoji: "🥷",
-//     title: "ninja",
-//   },
-//   {
-//     emoji: "🦁",
-//     title: "lion",
-//   },
-//   {
-//     emoji: "🐻",
-//     title: "bear",
-//   },
-//   {
-//     emoji: "👩‍⚕️",
-//     title: "doctor",
-//   },
-//   {
-//     emoji: "🤡",
-//     title: "clown",
-//   },
-//   {
-//     emoji: "🧛‍♂️",
-//     title: "dracula",
-//   },
-// ];
 
 // TODO: Remove dumb victory flag
 const victory = false;
